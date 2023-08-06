185ab0095895f076898bda40e5623c90ed481bec
Peshala Prabhapoorna <109937177+peshala-prabhapoorna@users.noreply.github.com>
2023-08-06 13:51:13 +0530

    Delete hello world 2/target directory

--- hello world 2/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Peshala Prabhapoorna\Documents\Rust\hello world 2\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\Users\Peshala Prabhapoorna\Documents\Rust\hello world 2\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- hello world 2/target/debug/hello-world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Peshala\ Prabhapoorna\Documents\Rust\hello\ world\ 2\target\debug\hello-world.exe: C:\Users\Peshala\ Prabhapoorna\Documents\Rust\hello\ world\ 2\src\main.rs
