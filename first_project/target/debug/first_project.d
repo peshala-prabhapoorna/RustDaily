f1dcff467737d196efa4182483f9b4f55d91c99c
Your Name <you@example.com>
2023-08-06 12:31:22 +0530

    .d commit

--- first_project/target/debug/first_project.d
+++ first_project/target/debug/first_project.d
@@ -1,1 +1,1 @@
-C:\Users\Peshala\ Prabhapoorna\Documents\Rust\first_project\target\debug\first_project.exe: C:\Users\Peshala\ Prabhapoorna\Documents\Rust\first_project\src\main.rs
+C:\Users\Peshala\ Prabhapoorna\Documents\rustdaily\first_project\target\debug\first_project.exe: C:\Users\Peshala\ Prabhapoorna\Documents\rustdaily\first_project\src\main.rs
