85b8970f143c7f2fc0dad903cf5018467076b9c1
Your Name <you@example.com>
2023-08-09 13:34:38 +0530

    Update bisection-method
    prompt user and get input for a, b, tolerance. Unsure about the algorithm, but runs and outputs a root.

--- bisection-method/src/main.rs
+++ bisection-method/src/main.rs
@@ -1,30 +1,36 @@
-use std::io::*;
 use evalexpr::eval;
+use std::io::*;
 
 fn main() {
-    let mut user_input = String::new();
+    let mut function_input = String::new();
     println!("Enter a function to find its root:");
     stdin()
-        .read_line(&mut user_input)
+        .read_line(&mut function_input)
         .expect("Failed to read line");
-
-    println!("Enter two values on either side of root and tolerance:");
-    println!("(insert a single space between each value)");
-    stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line");
+    let function_str: &str = function_input.trim();
     
-    let mut split_user_input= user_input.split(" ");
-    let a_str = split_user_input.next().unwrap();
-    let b_str = split_user_input.next().unwrap();
-    let tolerance_str = split_user_input.next().unwrap();
-    //println!("a: {} b: {} tolerance: {}", a_str, b_str, tolerance_str);
+    let mut a_input = String::new();
+    println!("Enter a value for a:");
+    stdin()
+        .read_line(&mut a_input)
+        .expect("Failed to read line");
+    let a: f64 = a_input.trim().parse().expect("Please type a number!");
 
-    let a: f64 = a_str.parse().unwrap();
-    let b: f64 = b_str.parse().unwrap();
-    let tolerance: f64 = tolerance_str.parse().unwrap();
-    //println!("a: {} b: {} tolerance: {}", a, b, tolerance);
-    println!("root: {}", bisection(&user_input, a, b, tolerance));
+    let mut b_input = String::new();
+    println!("Enter a value for b:");
+    stdin()
+        .read_line(&mut b_input)
+        .expect("Failed to read line");
+    let b: f64 = b_input.trim().parse().expect("Please type a number!");
+
+    let mut tolerance_input = String::new();
+    println!("Enter a value for tolerance:");
+    stdin()
+        .read_line(&mut tolerance_input)
+        .expect("Failed to read line");
+    let tolerance: f64 = tolerance_input.trim().parse().expect("Please type a number!");
+
+    println!("root: {}", bisection(&function_str, a, b, tolerance));
 }
 
 fn bisection(function_str: &str, mut a: f64, mut b: f64, tolerance: f64) -> f64 {
@@ -41,7 +47,7 @@ fn bisection(function_str: &str, mut a: f64, mut b: f64, tolerance: f64) -> f64 
 }
 
 fn evaluate_function(function_str: String, c: f64) -> f64 {
-    let function_c_str: String  = function_str.replace("x", &c.to_string());
+    let function_c_str: String = function_str.replace("x", &c.to_string());
     let function: evalexpr::Value = eval(&function_c_str).unwrap();
     function.as_float().unwrap()
 }
