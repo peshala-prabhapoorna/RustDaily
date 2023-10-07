27f4295b1784906d1848684e2b444b6bb93ebf5b
Your Name <you@example.com>
2023-10-07 18:51:43 +0530

    after a short break

---
+++ rust_web_yew/src/lib.rs
@@ -0,0 +1,24 @@
+use gloo::console::log;
+use yew::prelude::*;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct MyObject {
+    username: String,
+    favorite_language: String,
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let name: &str = "Choppar";
+    let my_object = MyObject {
+        username: name.to_owned(),
+        favorite_language: "Rust".to_owned(),
+    };
+
+    log!("my name is {}", name);
+    log!(serde_json::to_string_pretty(&my_object).unwrap());
+    html! {
+        <h1>{"Hello World!"}</h1>
+    }
+}
---
+++ rust_web_yew/src/main.rs
@@ -0,0 +1,5 @@
+use rust_web_yew::App;
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
