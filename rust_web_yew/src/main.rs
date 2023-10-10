d1dbba3b4fb9fb8b9a811b6481ec57e313e90bf8
Your Name <you@example.com>
2023-10-10 23:31:27 +0530

    fuck it

--- rust_web_yew/src/main.rs
+++
@@ -1,5 +0,0 @@
-use rust_web_yew::App;
-
-fn main() {
-    yew::Renderer::<App>::new().render();
-}
--- rust_web_yew_2/src/main.rs
+++
@@ -1,5 +0,0 @@
-use rust_web_yew_2::App;
-
-fn main() {
-    yew::Renderer::<App>::new().render();
-}
